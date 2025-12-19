613caae857a7707a5ca66b5f3d317cf7f818eff6
NeverSpot <rishuraj718@gmail.com>
2025-12-20 03:03:28 +0530

    folder refactor

--- backend/master.js
+++ backend/master.js
@@ -1,6 +1,22 @@
-import getDateForContest from "./cal.js ";
+import getDateForContest from "./cal.js";
+import mysql from "mysql2/promise";
+const contestId=2122;
 
 
+const db = await mysql.createConnection({
+    host: "localhost",
+    user: "nodeuser",
+    password: "insha",
+    database:"cf_data"
+});
 
-const a=await getDateForContest(2122);
-console.log(a)
+const a=await getDateForContest(contestId);
+
+for(const user of a){
+    await db.execute(`
+        INSERT INTO contest_results
+            (contest_id,handle,performance,delta,rating)
+
+        values (${contestId},${user.handle},${user.performance},${user.delta},${user.rating})
+    `)
+}
