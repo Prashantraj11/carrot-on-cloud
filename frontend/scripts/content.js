784ca6ff73d4d5e658b7f5df81b765b7ccae0988
Priyanshu Raj <rishuraj718@gmail.com>
2025-12-30 19:07:28 +0530

    local project completed, now i need to upload this to cloud

--- frontend/scripts/content.js
+++ frontend/scripts/content.js
@@ -14,18 +14,46 @@ const userList=new Set();
 // extracting participants username
 const table = document.querySelectorAll(".standings tbody .contestant-cell")
 for(const user of table){
-    let userName="";
-    for(const c of user.textContent){
-        if(c!==' ' && c!=='*' && c!=='#'){
-            userName+=c;
-        }
-    }
-    userList.add(userName);
+    userList.add(user.textContent.replace(/[ *#]/g, "").trim());
 }
 
 const queryData={contestId,userList:[...userList]};
 
+function updatePage(res){
+    const data={};
+    for(const user of res){
+        data[user.handle]={"delta":user.delta,"performance":user.performance};
+    }
+    const location =document.querySelector(".standings tr");
+    const pref=document.createElement("th")
+    const delta=document.createElement("th")
+    pref.textContent="Performance";
+    delta.textContent="Delta";
+    location.append(pref);
+    location.append(delta);
 
+    const table = document.querySelectorAll(".standings tbody tr");
+
+    for(let i=1;i<table.length-1;i++){
+        const user=table[i];
+        const handle=user.querySelector(".contestant-cell")
+
+        const userName=handle.textContent.replace(/[ *#]/g, "").trim();
+
+        if(data[userName]===undefined)continue;
+
+
+        const userPref=data[userName]["performance"];
+        const userDelta=data[userName]["delta"];
+
+        const pref=document.createElement("td")
+        const delta=document.createElement("td")
+        pref.textContent=userPref;
+        delta.textContent=userDelta;
+        user.append(pref);
+        user.append(delta);
+    }
+}
 
 fetch("http://127.0.0.1:3000/contest",{
     method:"POST",
@@ -36,6 +64,7 @@ fetch("http://127.0.0.1:3000/contest",{
 }).then(res=>res.json())
     .then((res)=> {
         console.log(res)
+        updatePage(res);
     }) .catch(error => {
     console.error('Error fetching data:', error); // Handle network errors
 });
